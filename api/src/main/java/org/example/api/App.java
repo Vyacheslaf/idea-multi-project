883a7d9087422e92fcf184edc1df9a7ff9298d0d
Vyacheslav Fedchenko <vyacheslav.fedchenko@gmail.com>
2023-04-06 21:16:05 +0300

    solved

--- api/src/main/java/org/example/api/App.java
+++ api/src/main/java/org/example/api/App.java
@@ -4,6 +4,7 @@ import org.example.core.Utils;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+        System.out.println("Running org.example.api.App.main");
+        System.out.println((Utils.isAllPositiveNumbers("12", "79") ? "A" : "Not a") + "ll numbers are positive!");
     }
 }
--- core/src/main/java/org/example/core/Utils.java
+++ core/src/main/java/org/example/core/Utils.java
@@ -2,13 +2,19 @@ package org.example.core;
 
 import org.example.StringUtils;
 
+import java.util.Arrays;
+
 public class Utils {
+
+    private Utils() {}
+
     public static boolean isAllPositiveNumbers(String... str) {
         if ((str == null) || (str.length == 0)) {
             throw new UnsupportedOperationException("There must be at least one argument and it cannot be null");
         }
+        String arrayString = Arrays.toString(str).replace("[", "").replace("]", "");
+        System.out.println("Running org.example.core.Utils.isAllPositiveNumbers(" + arrayString + ")");
         for (String s : str) {
-            System.out.println(s);
             if (!StringUtils.isPositiveNumber(s)) {
                 return false;
             }
